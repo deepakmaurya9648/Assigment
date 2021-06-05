0af9dcd85b7d8b8d6dbc70004965fc187edc2794
Deepak  Maurya <dkmaurya9648@gmail.com>
2021-06-05 08:59:48 +0530

    .

---
+++ javaScript tut/modules/module1.js
@@ -0,0 +1,7 @@
+import {message,user,test} from "./module2.js";
+
+console.log(message);
+
+user("hello ajeet");
+
+let a=new test();
---
+++ javaScript tut/modules/module2.js
@@ -0,0 +1,11 @@
+let message='hello world';
+function user(name){
+    console.log(name);
+}
+
+class test{
+    constructor(){
+        console.log('hello everyone');
+    }
+}
+export{message,user,test};
